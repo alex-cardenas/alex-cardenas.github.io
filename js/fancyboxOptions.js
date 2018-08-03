d5d0fdee35be3c65f2861e1cfff91349e9c7ac15
bust3ralex <cardenasa@carleton.edu>
2018-08-03 19:06:47 -0400

    Fixed JS link

--- js/fancyboxOptions.js
+++ js/fancyboxOptions.js
@@ -1,24 +1,22 @@
-<script type="text/javascript">
-        $(document).ready(function() {
-           $('[data-fancybox="gallery"]').fancybox({
-                loop: false,
-                gutter: 01,
-                buttons: [
-                    //"share",
-                    //"slideShow",
-                    "fullScreen",
-                    "download",
-                    "thumbs",
-                    "close"
-                ],
-                idleTime: 30,
-                animationEffect: "zoom",
-                transitionEffect: "slide",
-                clickContent: function(current, event) {
-                    return current.type === "image" ? "toggleControls" : false;
-                },
-               // Clicked on the slide
-                clickSlide: "close",
-            });
-        });
-</script>
+$(document).ready(function() {
+   $('[data-fancybox="gallery"]').fancybox({
+        loop: false,
+        gutter: 01,
+        buttons: [
+            //"share",
+            //"slideShow",
+            "fullScreen",
+            "download",
+            "thumbs",
+            "close"
+        ],
+        idleTime: 30,
+        animationEffect: "zoom",
+        transitionEffect: "slide",
+        clickContent: function(current, event) {
+            return current.type === "image" ? "toggleControls" : false;
+        },
+       // Clicked on the slide
+        clickSlide: "close",
+    });
+});
